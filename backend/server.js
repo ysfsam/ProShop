bd207caff768d96919da979b648da8ee37786fb9
Samuel Fanawopo <sfanawopo@gmail.com>
2023-05-31 14:58:45 +0100

    get products from database

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,20 @@
 import express from "express";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
-connectDB();  
+connectDB();
 
-const app = express();  
+const app = express();
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
