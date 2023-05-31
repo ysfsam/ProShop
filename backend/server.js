ab3a739e8b182c00387dc475d01878f499db6339
Samuel Fanawopo <sfanawopo@gmail.com>
2023-05-31 15:06:00 +0100

    custom error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import productRoutes from "./routes/productRoutes.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 
@@ -16,5 +17,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
