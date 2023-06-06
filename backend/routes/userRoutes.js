2ed800ce39d85b92930423faea70ea55311fc4da
Samuel Fanawopo <sfanawopo@gmail.com>
2023-06-06 23:06:56 +0100

    user routes + controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,8 +16,7 @@ const router = express.Router();
 router.route("/").post(registerUser).get(getUsers);
 router.post("/auth", authUser);
 router.post("/logout", logoutUser);
-router.route("/profile");
-
-router.route("/:id");
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser);
 
 export default router;
