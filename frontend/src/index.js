a3c06a8b687bf5ef14664ad2ef6dd6ec0eaa1ac0
Samuel Fanawopo <sfanawopo@gmail.com>
2023-06-08 22:33:18 +0100

    private routes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,8 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
+import PrivateRoute from "./components/PrivateRoute";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -29,6 +31,8 @@ const router = createBrowserRouter(
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
       <Route path="/profile" element={<ProfileScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
+      <Route path="" element={<PrivateRoute />} />
     </Route>
   )
 );
