89560a6a3c80bbb6b05084af80d97e4464e07fb1
Samuel Fanawopo <sfanawopo@gmail.com>
2023-06-09 16:08:14 +0100

    paypal integration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import {
   createBrowserRouter,
   createRoutesFromElements,
@@ -49,7 +50,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
